e37d2cba5aa36490bc9a153f96eccee119b5710a
Bohdan Oleksiichuk <39810144+Mercer13@users.noreply.github.com>
2023-01-31 12:56:05 +0200

    add sprites and script

---
+++ Assets/Scripts/Pig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    [SerializeField] private float stamina = 5f; // выносливость свиньи
+
+    private void OnCollisionEnter2D(Collision2D collision) // метод для столкновения
+    {
+        if (collision.relativeVelocity.magnitude > stamina) // если сила удара больше, чем выносливость
+        {
+            Destroy(gameObject); // то объект удаляется
+        }
+    }
+}
