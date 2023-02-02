751ea50d06e9a2849cc00edd59378377aaa4b7f7
Serhii Meleshchuk <meleshchuk_ak16@nuwm.edu.ua>
2023-02-02 03:30:40 +0200

    Add missile spawn

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pig : MonoBehaviour
+public class Missile : MonoBehaviour
 {
     [SerializeField] private float stamina = 5f; // выносливость свиньи
+    public float speed;
+    public Vector2 dir;
 
     private void OnCollisionEnter2D(Collision2D collision) // метод для столкновения
     {
@@ -13,5 +15,10 @@ public class Pig : MonoBehaviour
            // Destroy(gameObject); // то объект удаляется
            gameObject.SetActive(false);
         }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(speed * dir * Time.deltaTime, Space.World);
     }
 }
